edba7884e39ac88cf2c7cb7adac40f33e4fd49f0
NicoFilips <git@nicofilips.de>
2024-09-15 21:21:49 +0200

    feat: add editorconfig

--- MemeMuncherAI/MemeMuncherAI/DependencyInjection/ServiceCollectionExtensions.cs
+++ MemeMuncherAI/MemeMuncherAI/DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,17 +2,16 @@
 using MemeMuncherAI.Util.Abstraction;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MemeMuncherAI.DependencyInjection
-{
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddMemeMuncherServices(this IServiceCollection services)
-        {
-            services.AddTransient<IDirectoryUtil, DirectoryUtil>();
-            services.AddTransient<IFileUtil, FileUtil>();
-            services.AddTransient<IImageAnalyzerUtil, ImageAnalyzerUtil>();
+namespace MemeMuncherAI.DependencyInjection;
 
-            return services;
-        }
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMemeMuncherServices(this IServiceCollection services)
+    {
+        services.AddTransient<IDirectoryUtil, DirectoryUtil>();
+        services.AddTransient<IFileUtil, FileUtil>();
+        services.AddTransient<IImageAnalyzerUtil, ImageAnalyzerUtil>();
+
+        return services;
     }
 }
--- MemeMuncherAI/MemeMuncherAI/Program.cs
+++ MemeMuncherAI/MemeMuncherAI/Program.cs
@@ -3,41 +3,40 @@ using MemeMuncherAI.DependencyInjection;
 using MemeMuncherAI.Util.Abstraction;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MemeMuncherAI
+namespace MemeMuncherAI;
+
+class Program
 {
-    class Program
+    private const string CredentialPath = @"C:\Users\busin\Desktop\credentials.json";
+    private const string ImagePath = @"C:\Users\busin\Desktop\GCP-AI-Detect-Memes";
+    private static readonly string IsMemePath = Path.Combine(ImagePath, "IsMeme");
+    private static readonly string NotMemePath = Path.Combine(ImagePath, "NotMeme");
+
+    private static IDirectoryUtil? _directoryUtil = null;
+    private static IFileUtil? _fileUtil = null;
+    private static IImageAnalyzerUtil? _imageAnalyzerUtil = null;
+
+
+    private static void Main(string[] args)
     {
-        private const string CredentialPath = @"C:\Users\busin\Desktop\credentials.json";
-        private const string ImagePath = @"C:\Users\busin\Desktop\GCP-AI-Detect-Memes";
-        private static readonly string IsMemePath = Path.Combine(ImagePath, "IsMeme");
-        private static readonly string NotMemePath = Path.Combine(ImagePath, "NotMeme");
+        var serviceProvider = new ServiceCollection()
+                             .AddMemeMuncherServices()
+                             .BuildServiceProvider();
 
-        private static IDirectoryUtil? _directoryUtil = null;
-        private static IFileUtil? _fileUtil = null;
-        private static IImageAnalyzerUtil? _imageAnalyzerUtil = null;
+        _directoryUtil = serviceProvider.GetService<IDirectoryUtil>();
+        _fileUtil = serviceProvider.GetService<IFileUtil>();
+        _imageAnalyzerUtil = serviceProvider.GetService<IImageAnalyzerUtil>();
 
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", CredentialPath);
+        var client = ImageAnnotatorClient.Create();
 
-        private static void Main(string[] args)
+        _directoryUtil?.CreateDirectories(IsMemePath, NotMemePath);
+
+        foreach (var file in Directory.EnumerateFiles(ImagePath))
         {
-            var serviceProvider = new ServiceCollection()
-                .AddMemeMuncherServices()
-                .BuildServiceProvider();
-
-            _directoryUtil = serviceProvider.GetService<IDirectoryUtil>();
-            _fileUtil = serviceProvider.GetService<IFileUtil>();
-            _imageAnalyzerUtil = serviceProvider.GetService<IImageAnalyzerUtil>();
-
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", CredentialPath);
-            var client = ImageAnnotatorClient.Create();
-
-            _directoryUtil?.CreateDirectories(IsMemePath, NotMemePath);
-
-            foreach (var file in Directory.EnumerateFiles(ImagePath))
-            {
-                var image = Image.FromFile(file);
-                bool isMeme = _imageAnalyzerUtil!.IsMemeImage(client, image, file);
-                _fileUtil?.MoveFile(file, isMeme ? IsMemePath : NotMemePath, isMeme);
-            }
+            var image = Image.FromFile(file);
+            bool isMeme = _imageAnalyzerUtil!.IsMemeImage(client, image, file);
+            _fileUtil?.MoveFile(file, isMeme ? IsMemePath : NotMemePath, isMeme);
         }
     }
 }
